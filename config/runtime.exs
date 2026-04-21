d30861c5aed28c5d0f34a13b115a8bc39e1f5bb4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2026-04-20 19:46:57 -0700

    Switch aria_storage and ex_aws to split host/port config keys

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,7 +2,17 @@ import Config
 
 config :aria_storage,
   storage_backend: :s3,
-  s3_bucket: System.get_env("AWS_S3_BUCKET", "uro-uploads"),
-  s3_endpoint: System.get_env("AWS_S3_ENDPOINT", "http://localhost:7070"),
-  aws_access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
-  aws_secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY")
+  bucket:             System.get_env("AWS_S3_BUCKET",    "uro-uploads"),
+  region:             System.get_env("AWS_REGION",       "us-east-1"),
+  access_key_id:      System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key:  System.get_env("AWS_SECRET_ACCESS_KEY")
+
+config :ex_aws,
+  access_key_id:     System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region:            System.get_env("AWS_REGION", "us-east-1"),
+  s3: [
+    scheme:   "http://",
+    host:     System.get_env("AWS_S3_HOST", "localhost"),
+    port:     String.to_integer(System.get_env("AWS_S3_PORT", "7070"))
+  ]
